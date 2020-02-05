7e43494a366c7cde6fb210b9c9f7a1af30b0e5dd
rayanprasanna <rayanprasanna224@gmail.com>
2020-02-05 06:33:28 +0530

    Depala new Version

---
+++ app/src/main/java/com/universl/realestate/helper/AppHelper.java
@@ -0,0 +1,44 @@
+package com.universl.realestate.helper;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Sketch Project Studio
+ * Created by Ryan on 11/20/2019 14.27.
+ */
+public class AppHelper {
+
+    /**
+     * Turn drawable resource into byte array.
+     *
+     * @param context parent context
+     * @param id      drawable resource id
+     * @return byte array
+     */
+    public static byte[] getFileDataFromDrawable(Context context, int id) {
+        Drawable drawable = ContextCompat.getDrawable(context, id);
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    /**
+     * Turn drawable into byte array.
+     *
+     * @param drawable data
+     * @return byte array
+     */
+    public static byte[] getFileDataFromDrawable(Context context, Drawable drawable) {
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+}
